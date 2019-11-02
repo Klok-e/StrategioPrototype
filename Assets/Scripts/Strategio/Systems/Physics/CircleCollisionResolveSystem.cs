de4c014a8b0c4717ea8622b826fd244e56db21d5
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2019-11-02 14:53:30 +0200

    add collision resolving

--- Assets/Scripts/Strategio/Systems/Physics/CircleCollisionResolveSystem.cs
+++ Assets/Scripts/Strategio/Systems/Physics/CircleCollisionResolveSystem.cs
@@ -1,13 +1,55 @@
-﻿using Unity.Entities;
+﻿using Strategio.Components.Physics;
+using Unity.Burst;
+using Unity.Entities;
 using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 namespace Strategio.Systems.Physics
 {
-    public class CircleCollisionResolveSystem:JobComponentSystem
+    public class CircleCollisionResolveSystem : JobComponentSystem
     {
+        public const float RepulsionPower = 0.08f;
+
+        private EndSimulationEntityCommandBufferSystem _barrier;
+        //private EntityQuery _query;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            _barrier = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+            //_query = EntityManager.CreateEntityQuery(ComponentType.ReadWrite<Translation>());
+        }
+
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
-            return inputDeps;
+            var j1 = new ResolveCollisionEventsJob
+            {
+                commandBuffer = _barrier.CreateCommandBuffer().ToConcurrent(),
+                entityTranslations = GetComponentDataFromEntity<Translation>(),
+            };
+            // TODO: fix this schedule single
+            var h1 = j1.ScheduleSingle(this, inputDeps);
+            _barrier.AddJobHandleForProducer(h1);
+            return h1;
+        }
+
+        [BurstCompile]
+        private struct ResolveCollisionEventsJob : IJobForEachWithEntity_EC<CollisionComponent>
+        {
+            public EntityCommandBuffer.Concurrent commandBuffer;
+            public ComponentDataFromEntity<Translation> entityTranslations;
+
+            public void Execute(Entity entity, int index, ref CollisionComponent c0)
+            {
+                var ent1 = entityTranslations[c0.ent1];
+                var ent2 = entityTranslations[c0.ent2];
+                ent1.Value -= math.float3(c0.ent1ToEnt2Dir, 0f) * RepulsionPower;
+                ent2.Value += math.float3(c0.ent1ToEnt2Dir, 0f) * RepulsionPower;
+                entityTranslations[c0.ent1] = ent1;
+                entityTranslations[c0.ent2] = ent2;
+                commandBuffer.DestroyEntity(index, entity);
+            }
         }
     }
 }
