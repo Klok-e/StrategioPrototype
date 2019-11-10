b3b95bdeccfcaa4513e5b2656ea6deca65d8d2af
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2019-11-10 14:17:03 +0200

    spawners now spawn new units at some distance from themselves

--- Assets/Scripts/Strategio/Systems/UnitSpawnerSystem.cs
+++ Assets/Scripts/Strategio/Systems/UnitSpawnerSystem.cs
@@ -1,28 +1,33 @@
 ﻿using System;
 using Strategio.Components;
+using Strategio.Components.Physics;
 using Strategio.GameConfigs;
 using Unity.Entities;
 using Unity.Jobs;
+using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.WSA;
+using Random = Unity.Mathematics.Random;
 
 namespace Strategio.Systems
 {
     public class UnitSpawnerSystem : ComponentSystem
     {
         private InitDataSystem _dataSystem;
+        private Random _random;
 
         protected override void OnCreate()
         {
             base.OnCreate();
             _dataSystem = World.GetOrCreateSystem<InitDataSystem>();
+            _random = new Random(42);
         }
 
         protected override void OnUpdate()
         {
             Entities.ForEach((ref SpawnerComponent spawner, ref SideComponent sideComponent,
-                              ref Translation translation) =>
+                              ref Translation translation, ref CircleColliderComponent collider) =>
             {
                 if (spawner.spawnProgress > spawner.unitType.GetStats().requiredProgressToSpawn)
                 {
@@ -30,7 +35,12 @@ namespace Strategio.Systems
 
                     var ent = PostUpdateCommands.CreateEntity(spawner.unitType.GetArchetype(_dataSystem.archetypes));
                     var config = _dataSystem.GetConfigForUnitType(spawner.unitType, sideComponent.side);
-                    UnitTypeUtil.SetCommonConfigComponentsToEntity(PostUpdateCommands, ent, config, translation.Value,
+                    float minSpawnDist = config.colliderRadius + collider.radius;
+                    float dist = _random.NextFloat(minSpawnDist, minSpawnDist + minSpawnDist * 0.5f);
+                    float angle = _random.NextFloat(0f, math.PI * 2);
+                    var offset = math.float2(math.cos(angle), math.sin(angle)) * dist;
+                    var pos = translation.Value + math.float3(offset, 0f);
+                    UnitTypeUtil.SetCommonConfigComponentsToEntity(PostUpdateCommands, ent, config, pos,
                         sideComponent, spawner.unitType);
                 }
 
